3bd0c3667feca3f6443f67257c215504911a756a
Pawel Budzynski <budzynski.pawel@gmail.com>
2018-09-06 17:45:04 +0200

    responsive menu added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // alert("DOMContentLoaded");
+  var hamburger = document.getElementById('hamburger');
+var menu = document.getElementById("menu");
+hamburger.addEventListener("click", function onClick(event) {
+    hamburger.classList.toggle('hamburger-active');
+  menu.classList.toggle("center-menu");
+    
+  });
 })
