e93b43225f56ed45377cfd27e87abb18eac1cad1
Pawel Budzynski <budzynski.pawel@gmail.com>
2018-09-13 17:35:48 +0200

    gallery working

--- js/app.js
+++ js/app.js
@@ -5,5 +5,46 @@ hamburger.addEventListener("click", function onClick(event) {
     hamburger.classList.toggle('hamburger-active');
   menu.classList.toggle("center-menu");
     
+  
+  
   });
+  // gallery
+  
+  $left = $('#gal-left');
+  $right = $('#gal-right');
+  $left.on('click', backGal);
+  $right.on('click', forwardGal);
+
+  $items = $('[class^=chair-]');
+
+  $visible = 0;
+  function backGal(){
+    if($visible !==0){
+      $visible--;
+    }else{
+      $visible = $items.length - 1;
+    }
+    
+    console.log('left');
+    $items.css('opacity', '0');
+    
+    $($items[$visible]).css('opacity','1');
+    
+    
+  };
+  function forwardGal(){
+    if($visible !== $items.length - 1){
+      $visible++;
+    }else{
+      $visible = 0;
+    }
+    
+    console.log('right');
+    console.log($visible);
+    $items.css('opacity', '0');
+    
+    $($items[$visible]).css('opacity','1');
+  }
+
+  // setInterval(forwardGal, 8000);
 })
